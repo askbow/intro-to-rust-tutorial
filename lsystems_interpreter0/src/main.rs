8b8ba6bbaf45f6012021326e6b4e88c853cc99cd
Denis Borchev <askbow@users.noreply.github.com>
2023-06-18 12:02:54 +0200

    try interpreter-zero

--- lsystems_interpreter0/src/main.rs
+++ lsystems_interpreter0/src/main.rs
@@ -2,10 +2,12 @@ use turtle::*;
 
 fn main() {
     let mut fred = Turtle::new();
+    // double-quotes is a string:
     let pattern = "F+F+F+F";
 
     for character in pattern.chars() {
         match character {
+            // single-quotes is chars:
             'F' => fred.forward(50.0),
             '+' => fred.left(90.0),
             _ => continue,
