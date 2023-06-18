fa91286dc8d73555d9a7cb94177e7e7d7aa1dcdc
Denis Borchev <askbow@users.noreply.github.com>
2023-06-18 11:26:05 +0200

    add a better rng

--- basic/rand.rs
+++ basic/rand.rs
@@ -1,7 +1,21 @@
-pub fn rand() -> usize {
+pub fn _rand() -> usize {
     42
 }
 
+pub fn rand(seed: usize) -> usize {
+    let mut x = seed; // mutable
+    /*
+    pseudo-random number
+    doi:10.18637/jss.v008.i14
+    */
+    x ^= x << 21;
+    x ^= x >> 35;
+    x ^= x << 4;
+    // last value is the return:
+    x
+}
+
 fn main() {
-    rand();
+    // format with a string literal
+    println!("{}", rand(42));
 }
